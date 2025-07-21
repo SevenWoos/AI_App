2c6db44dcea6e34ccdaa77d352e68a12dd2e49c7
7Woos <swenwu96@gmail.com>
2025-07-21 16:02:25 -0700

    fixed database bug

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ from flask_bcrypt import Bcrypt
 app = Flask(__name__)
 bcrypt = Bcrypt(app)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///databse.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 app.config['SECRET_KEY'] = 'thisisasecretkey'
 db = SQLAlchemy(app)
 
